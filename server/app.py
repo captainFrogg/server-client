b1c93e94207395a6814223488292c868876365a3
Romain <romain.texier@mhcomm.fr>
2023-02-20 09:37:08 +0100

    create database and user model with sql-alchemy

--- server/app.py
+++ server/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from database.database import db_session, init_db
 
 app = Flask(__name__)
+init_db()
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 @app.route("/")
 def hello_world():
